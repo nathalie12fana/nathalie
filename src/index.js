525bcdf32d0821742974a7413baf6e043f65505e
nathalie12fana <christellenathaliefana@gmail.com>
2025-05-24 08:47:54 +0100

    bijou-project

---
+++ src/index.js
@@ -0,0 +1,16 @@
+let main = document.querySelector('#menu-btn');
+let menu = document.querySelector('.menu');
+const burgerMenu=document.querySelector("#burgerMenu")
+const Menu=document.querySelector(".menu")
+main.onclick = () =>{
+    main.classList.toggle('fa-times')
+     Menu.classList.toggle('none')
+
+}
+burgerMenu.addEventListener('click',()=>{
+
+
+
+})
+
+
